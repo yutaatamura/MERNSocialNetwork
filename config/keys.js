47f6e7a681105185737c274e859f64aa4548e71d
yutaatamura <yutaatamura@gmail.com>
2018-08-28 10:29:55 -0500

    Setup validation

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://amida:amida1@ds159997.mlab.com:59997/karmadevconnector"
+  mongoURI: "mongodb://amida:amida1@ds159997.mlab.com:59997/karmadevconnector",
+  secretOrKey: "secret"
 };
