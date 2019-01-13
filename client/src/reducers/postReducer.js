d53a35713a3b1d1733a855208d43f7fa0a4ad57f
yutaatamura <yutaatamura@gmail.com>
2019-01-12 20:35:50 -0600

    postreducer edit

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -39,7 +39,7 @@ export default function(state = initialState, action) {
     case DELETE_POST:
       return {
         ...state,
-        posts: state.posts.filter(post => post._id !=== action.payload)
+        posts: state.posts.filter(post => post._id !== action.payload)
       };
     default:
       return state;
