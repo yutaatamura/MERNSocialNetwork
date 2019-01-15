c04c2913d85d066a38bc9f8a52cebebe71f6f3cb
yutaatamura <yutaatamura@gmail.com>
2019-01-14 20:52:29 -0600

    Update profilegithub

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -6,8 +6,8 @@ class ProfileGithub extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      clientId: "26c196bacea7db10cf48",
-      clientSecret: "0885cb690e07d2a93a6afb0891fb552fd9f7aa53",
+      clientId: "",
+      clientSecret: "",
       count: 5,
       sort: "created: asc",
       repos: []
